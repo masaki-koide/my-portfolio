5c193554278779f566b8429eaa2d9de571a6c527
masaki.koide <ikasama0324@gmail.com>
2018-10-19 22:42:50 +0900

    feat: [WIP] Qiitaから記事を取得

--- src/app/pages/about.tsx
+++ src/app/pages/about.tsx
@@ -2,25 +2,72 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 import App from '../components/App'
-import { AppState, incrementCount } from '../store'
+import { AppState, incrementCount, setQiitaItems } from '../store'
 
-const mapStateToProps = ({ count }: AppState) => ({
-  count
+const mapStateToProps = ({ count, qiitaItems }: AppState) => ({
+  count,
+  qiitaItems
 })
 
+interface APIResponse {
+  json?: any
+  error?: any
+}
+
+const wrapFetch = (
+  input: RequestInfo,
+  init?: RequestInit | undefined
+): Promise<APIResponse> => {
+  return fetch(input, init)
+    .then(response => response.json())
+    .then(json => {
+      return { json }
+    })
+    .catch(error => {
+      return { error }
+    })
+}
+
 const mapDispatchToProps = (dispatch: Dispatch) => {
-  return bindActionCreators({ incrementCount }, dispatch)
+  return {
+    ...bindActionCreators({ incrementCount }, dispatch),
+    async api() {
+      const items = await wrapFetch(
+        'https://qiita.com/api/v2/authenticated_user/items?per_page=5',
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.QIITA_ACCESS_TOKEN}`
+          }
+        }
+      )
+      if (items.json && !items.error) {
+        dispatch(setQiitaItems(items.json))
+      }
+    }
+  }
 }
 
 type Props = AppState & {
   incrementCount: () => void
+  api: () => Promise<any>
 }
 
-const about: React.SFC<Props> = ({ count, incrementCount }) => (
+const about: React.SFC<Props> = ({
+  count,
+  incrementCount,
+  api,
+  qiitaItems
+}) => (
   <App>
     <p>About Page</p>
     <button onClick={incrementCount}>おしてね</button>
     <p>{count}</p>
+    <button onClick={api}>おせよ</button>
+    <ul>
+      {qiitaItems.map(item => {
+        return <li key={item.id}>{item.title}</li>
+      })}
+    </ul>
   </App>
 )
 
--- src/app/store.ts
+++ src/app/store.ts
@@ -6,24 +6,31 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers'
 
 export interface AppState {
   count: number
+  qiitaItems: any[]
 }
 
 const initialState: AppState = {
-  count: 0
+  count: 0,
+  qiitaItems: []
 }
 
 const actionCreator = actionCreatorFactory()
 export const incrementCount = actionCreator('increment')
+export const setQiitaItems = actionCreator<any[]>('setQiitaItems')
 
-export const reducer = reducerWithInitialState<AppState>(initialState).case(
-  incrementCount,
-  state => {
+export const reducer = reducerWithInitialState<AppState>(initialState)
+  .case(incrementCount, state => {
     return {
       ...state,
       count: ++state.count
     }
-  }
-)
+  })
+  .case(setQiitaItems, (state, payload) => {
+    return {
+      ...state,
+      qiitaItems: payload
+    }
+  })
 
 export const initStore = (
   state: AppState = initialState,
