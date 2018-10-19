07462306708143cb5aeddbd1c4d36fc85ead3eb0
masaki-koide <ikasama0324@gmail.com>
2018-10-19 18:41:09 +0900

    chore: dotenvの設定

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -1,4 +1,15 @@
-const withTypescript = require('@zeit/next-typescript')
+const withTypescript = require("@zeit/next-typescript");
+const webpack = require("webpack");
+
+const { parsed: localEnv } = require("dotenv").config({
+  path: process.env.NODE_ENV === "production" ? ".env.production" : ".env"
+});
+
 module.exports = withTypescript({
-  distDir: '../../dist/functions/next'
-})
+  distDir: "../../dist/functions/next",
+  webpack: config => {
+    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+
+    return config;
+  }
+});
