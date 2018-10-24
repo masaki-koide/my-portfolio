d797302922fbe04a1e623518d5a36d41722309df
masaki.koide <ikasama0324@gmail.com>
2018-10-24 22:12:15 +0900

    feat: [WIP] Marterial-UIでスワイプカードを実装

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,6 +9,11 @@ const Header: React.SFC<{ pathname?: string }> = ({ pathname }) => (
     <Link href="/about">
       <a className={pathname === '/about' ? 'is-active' : ''}>About</a>
     </Link>
+    <Link href="/materialui">
+      <a className={pathname === '/materialui' ? 'is-active' : ''}>
+        MaterialUI
+      </a>
+    </Link>
   </header>
 )
 
