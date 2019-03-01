86648d65ec72081eeb18f8127f338d46fa74e3f5
masaki.koide <ikasama0324@gmail.com>
2019-03-01 21:11:46 +0900

    fix: 不要なmiddlewareを削除

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,6 @@
 import withRedux from 'next-redux-wrapper'
-import { applyMiddleware, createStore } from 'redux'
+import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import myMiddleware from '~/middleware'
 import reducer, { AppState } from '~/modules'
 
 export const initStore = (state: AppState, { debug }: withRedux.Options) => {
@@ -9,7 +8,7 @@ export const initStore = (state: AppState, { debug }: withRedux.Options) => {
     ? createStore(
         reducer,
         state,
-        composeWithDevTools(applyMiddleware(myMiddleware))
+        composeWithDevTools()
       )
-    : createStore(reducer, state, applyMiddleware(myMiddleware))
+    : createStore(reducer, state)
 }
