ed5eebee33e8e65f47bb16081258c0a7e4033809
masaki.koide <ikasama0324@gmail.com>
2019-03-05 22:25:33 +0900

    style: section-areaを100vhに変更

--- src/app/components/organisms/section-area.tsx
+++ src/app/components/organisms/section-area.tsx
@@ -3,7 +3,7 @@ import { FC, ReactNode } from 'react'
 
 const SectionArea = styled('section')`
   width: 100%;
-  min-height: 50vh;
+  min-height: 100vh;
   background-color: ${props => props.color};
 `
 
